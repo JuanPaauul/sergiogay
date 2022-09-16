5b3a3122c62db2f149775d96951633c62b7dc62e
Juan Pablo Carrasco Padilla <JOMPOL_JP@hotmail.com>
2022-09-16 15:39:00 -0400

    Un cambio

--- Problem A.cpp
+++ Problem A.cpp
@@ -3,11 +3,14 @@
 #include <string>
 using namespace std;
 
-int main() {
-  map<int, string> sample_map { { 1, "one"}, { 3, "two" } };
+int main()
+{
+  map<int, string> sample_map{{1, "one"}, {3, "two"}};
   cout << sample_map[1] << " " << sample_map[4] << endl;
   sample_map.insert(pair<int, string>(4, "four"));
   cout << sample_map[1] << " " << sample_map[4] << endl;
   sample_map[5] = "five";
   cout << sample_map[1] << " " << sample_map[5] << endl;
+
+  cout << "Estamos haciendo un trabajo a parte";
 }
